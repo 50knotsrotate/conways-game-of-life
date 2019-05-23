3434db48621b8cadf9c1648a17a95e489a71105b
= <=>
2019-05-23 15:38:30 -0700

    added pause and flip cell onclick

--- gol.js
+++ gol.js
@@ -1,7 +1,15 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d"); // g
 var grid = [];
-const rows = 80;
+const rows = 20;
+var gameplay = false; //For pausing
+
+document.addEventListener('keydown', (e) => { 
+  if (e.keyCode == '32') { 
+    gameplay = !gameplay
+  }
+})
+
 
 class Cell {
   constructor(x, y, size, current, newVal, pos) {
@@ -34,7 +42,7 @@ function createGrid() {
         j * (canvas.width / rows),
         i * (canvas.height / rows),
         canvas.width / rows,
-        Math.random() > 0.4,
+        false,
         null,
         { x: i, y: j }
       );
@@ -45,13 +53,27 @@ function createGrid() {
 }
 createGrid();
 
+canvas.addEventListener("click", e => {
+  // var x = Math.floor((e.clientX - 8) / (canvas.width / rows) / (3));
+  // var y = Math.floor(e.clientY / (canvas.height / rows) / 3);
+  
+  var x = Math.floor((e.clientX - 8) / (canvas.width / rows) /3 ) 
+  var y = Math.floor((e.clientY - 8) / (canvas.width / rows) / 3)
+  
+  grid[y][x].current = !grid[y][x].current;
+  //Each pixel is(canvas.PROP/rows long)
+  // console.log(Math.floor( ((( e.clientX - 8) / (canvas.width/rows)) /3 )) )
+  // console.log(canvas.width/rows)
+});
+
+
 function drawGrid() {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < rows; j++) {
    
       if (grid[j][i].current) {
            context.beginPath();
-           context.fillStyle = "white";
+           context.fillStyle = "black";
         context.rect(
           grid[j][i].x,
           grid[j][i].y,
@@ -62,7 +84,7 @@ function drawGrid() {
          context.stroke()
       } else { 
              context.beginPath();
-             context.fillStyle = "black";
+             context.fillStyle = "white";
              context.rect(
                grid[j][i].x,
                grid[j][i].y,
@@ -78,21 +100,25 @@ function drawGrid() {
 }
 
 function go() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  //check neighbors
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[j][i].update();
+    
+  if (gameplay) {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    //check neighbors
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < rows; j++) {
+        grid[j][i].update();
+      }
     }
-  }
 
-  //set new to current
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < rows; j++) {
-      grid[j][i].current = grid[j][i].newVal;
-      grid[j][i].newVal = null;
+    //set new to current
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < rows; j++) {
+        grid[j][i].current = grid[j][i].newVal;
+        grid[j][i].newVal = null;
+      }
     }
   }
-  drawGrid();
+  
+drawGrid();
 }
 setInterval(go, 75);
